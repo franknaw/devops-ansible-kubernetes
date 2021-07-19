f1c850064d0f2b7e544b2c74dc6e07935598cd96
tad <franknaw@gmail.com>
2021-07-19 14:51:29 -0400

    Handle replica out of sync

---
+++ ansible/scripts/reset-mongo-data.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if [ $# -eq 0 ]; then
+    echo "Please input the host name of the server"
+    exit 1
+fi
+
+echo "Using Host $1"
+
+ANSIBLE_STDOUT_CALLBACK=yaml ansible-playbook ~/A2T/dev/devops-ansible-kubernetes/ansible/playbooks/reset-mongo-data.yml --ask-vault-password --extra-vars '@passwd.yml' -e "theServer=$1"
+# -e "theServer=$1"
+
